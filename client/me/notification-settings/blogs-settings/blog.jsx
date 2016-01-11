fe335fb29afbd2dc75c617e87284868fd6c3da20
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-01-11 12:44:37 -0300

    Settings panel now opens by dafault for user with only one blog

--- client/me/notification-settings/blogs-settings/blog.jsx
+++ client/me/notification-settings/blogs-settings/blog.jsx
@@ -8,6 +8,7 @@ import classNames from 'classnames';
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
 import FoldableCard from 'components/foldable-card';
 import Header from './header';
 import SettingsForm from 'me/notification-settings/settings-form';
@@ -21,28 +22,28 @@ export default React.createClass( {
 		disableToggle: PropTypes.bool,
 		settings: PropTypes.instanceOf( Immutable.Map ).isRequired,
 		hasUnsavedChanges: PropTypes.bool.isRequired,
-		// onToggle: PropTypes.func.isRequired,
+		onToggle: PropTypes.func.isRequired,
 		onSave: PropTypes.func.isRequired,
 		onSaveToAll: PropTypes.func.isRequired
 	},
 
-	getInitialState() {
-		return {
-			isExpanded: false
-		};
-	},
-
 	render() {
 		const { blog, blog: { ID: sourceId }, settings, disableToggle, devices, hasUnsavedChanges, onToggle, onSave, onSaveToAll } = this.props;
-		const { isExpanded } = this.state;
-		const styles = classNames( 'notification-settings-blog-settings-blog', {
-			'is-compact': ! isExpanded,
-			'is-expanded': isExpanded
-		} );
+
+		if ( disableToggle ) {
+			return (
+				<Card className="notification-settings-blog-settings-blog is-compact">
+					<Header { ...{ blog, settings } } />
+					<SettingsForm
+						{ ...{ sourceId, devices, settings, hasUnsavedChanges, isApplyAllVisible: ! disableToggle, onToggle, onSave, onSaveToAll } }
+						settingKeys={ [ 'new_comment', 'comment_like', 'post_like', 'follow', 'achievement', 'mentions' ] } />
+				</Card>
+			);
+		}
 
 		return (
 			<FoldableCard
-				className={ styles }
+				className="notification-settings-blog-settings-blog is-compact"
 				header={ <Header { ...{ blog, settings } } /> } >
 					<SettingsForm
 						{ ...{ sourceId, devices, settings, hasUnsavedChanges, isApplyAllVisible: ! disableToggle, onToggle, onSave, onSaveToAll } }
