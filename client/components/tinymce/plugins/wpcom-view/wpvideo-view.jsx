a3bff2a7a2c39968095322d6675f052d01caed78
Miklos Juhasz <juhasz.miklos.1@gmail.com>
2016-01-31 20:12:58 +0100

    Split monolithic component into a presentational and a container one.
    This is to seperate the concerns of fetching the data and visualizing it.

--- client/components/tinymce/plugins/wpcom-view/wpvideo-view.jsx
+++ client/components/tinymce/plugins/wpcom-view/wpvideo-view.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import omit from 'lodash/object/omit';
 import find from 'lodash/collection/find';
 import QueryString from 'querystring';
@@ -11,76 +11,17 @@ import tinymce from 'tinymce/tinymce';
  * Internal dependencies
  */
 import shortcodeUtils from 'lib/shortcode';
-import MediaActions from 'lib/media/actions';
-import MediaListStore from 'lib/media/list-store';
 import MediaUtils from 'lib/media/utils';
-
-const getStateData = siteId => {
-	return {
-		media: MediaListStore.getAll( siteId ),
-		mediaHasNextPage: MediaListStore.hasNextPage( siteId ),
-		mediaFetchingNextPage: MediaListStore.isFetchingNextPage( siteId )
-	};
-};
 
 class WpVideoView extends Component {
 
-	static match( content ) {
-		const match = shortcodeUtils.next( 'wpvideo', content );
-
-		if ( match ) {
-			return {
-				index: match.index,
-				content: match.content,
-				options: {
-					shortcode: match.shortcode
-				}
-			};
-		}
-	}
-
-	static serialize( content ) {
-		return encodeURIComponent( content );
-	}
-
 	constructor( props ) {
 		super( props );
-		this.state = getStateData( props.siteId );
-	}
-
-	componentDidMount() {
-		MediaListStore.on( 'change', this.updateStateData.bind( this ) );
-		this.fetchData( this.props.siteId );
-	}
-
-	componentWillUnmount() {
-		MediaListStore.off( 'change', this.updateStateData.bind( this ) );
-	}
-
-	componentWillReceiveProps( nextProps ) {
-		if ( this.props.siteId !== nextProps.siteId ) {
-			this.fetchData( this.props.siteId );
-			this.updateStateData();
-		}
-	}
-
-	fetchData( siteId ) {
-		setTimeout( () => {
-			MediaActions.setQuery( siteId, { mime_type: 'video/' } );
-			MediaActions.fetchNextPage( siteId )
-		}, 0 );
-	}
-
-	updateStateData() {
-		this.setState( getStateData( this.props.siteId ) );
-		if ( ! this.state.mediaFetchingNextPage && this.state.mediaHasNextPage ) {
-			this.fetchData( this.props.siteId );
-		}
 	}
 
 	getVideoAttributes( videopress_guid ) {
-		if ( this.state.media ) {
-			return find( this.state.media, m => MediaUtils.isVideoPressItem( m ) && m.videopress_guid === videopress_guid );
+		if ( this.props.media ) {
+			return find( this.props.media, m => MediaUtils.isVideoPressItem( m ) && m.videopress_guid === videopress_guid );
 		}
 	}
 
@@ -174,5 +115,10 @@ class WpVideoView extends Component {
 	}
 
 }
+
+WpVideoView.propTypes = {
+	media: PropTypes.array,
+	content: PropTypes.string
+};
 
 export default WpVideoView;
