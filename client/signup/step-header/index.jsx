25991a4a3be776249a95275dc1e65aa03d628690
alternatekev <alternatekev@me.com>
2016-02-03 14:55:14 -0700

    added button to theme selection screen in start, added placeholder connect block
    Jetpack-connect: Create routes & main component
    
    added button to theme selection screen in start, added placeholder connect block
    
    Jetpack-connect: Create routes & main component
    
    added button to theme selection screen in start, added placeholder connect block
    
    Jetpack-connect: Create routes & main component
    
    added button to theme selection screen in start, added placeholder connect block

--- client/signup/step-header/index.jsx
+++ client/signup/step-header/index.jsx
@@ -1,21 +1,29 @@
 /**
  * External dependencies
  */
-var React = require( 'react' );
+import React, { PropTypes } from 'react';
 
 /**
  * Internal dependencies
  */
-var preventWidows = require( 'lib/formatting' ).preventWidows;
+import { preventWidows } from 'lib/formatting';
+import Button from 'components/button';
 
 module.exports = React.createClass( {
 	displayName: 'StepHeader',
+
+	propTypes: {
+		headerText: PropTypes.string,
+		subHeaderText: PropTypes.string,
+		showJetpackButton: PropTypes.bool
+	},
 
 	render: function() {
 		return (
 			<header className="step-header">
 				<h1 className="step-header__title">{ preventWidows( this.props.headerText, 2 ) }</h1>
 				<p className="step-header__subtitle">{ preventWidows( this.props.subHeaderText, 2 ) }</p>
+				<Button compact href="/jetpack/connect">{ this.translate( 'Or host your own site with Jetpack' ) }</Button>
 			</header>
 		);
 	}
