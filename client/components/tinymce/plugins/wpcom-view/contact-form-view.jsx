fa80c0a66d78bfbc1dbe416b7d8d4b2b234074de
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-02-12 14:09:01 -0300

    Editor: Contact Form Preview

--- client/components/tinymce/plugins/wpcom-view/contact-form-view.jsx
+++ client/components/tinymce/plugins/wpcom-view/contact-form-view.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-
+import { deserialize } from '../contact-form/shortcode-utils';
 /**
  * Internal dependecies
  */
@@ -32,10 +32,78 @@ export default React.createClass( {
 			editor.execCommand( 'wpcomContactForm', content );
 		}
 	},
+
+	renderContent() {
+		const required = required => required ? <em>&nbsp;({ this.translate( 'required' ) })</em> : null;
+		const textField = ( field, index ) => {
+			return (
+				<fieldset key={ 'contact-form-field-' + index } className="wpview-type-contact-form-field">
+					<legend>{ field.label }{ required( field.required ) }</legend>
+					<input type="text" />
+				</fieldset>
+			);
+		};
+		const textarea = ( field, index ) => {
+			return (
+				<fieldset key={ 'contact-form-field-' + index } className="wpview-type-contact-form-field">
+					<legend>{ field.label }{ required( field.required ) }</legend>
+					<textarea></textarea>
+				</fieldset>
+			);
+		};
+		const checkbox = ( field, index ) => {
+			return (
+				<fieldset key={ 'contact-form-field-' + index } className="wpview-type-contact-form-field">
+					<label><input type="checkbox" />{ field.label }{ required( field.required ) }</label>
+				</fieldset>
+			);
+		};
+		const dropdown = ( field, index ) => {
+			return (
+				<fieldset key={ 'contact-form-field-' + index } className="wpview-type-contact-form-field">
+					<legend>{ field.label }{ required( field.required ) }</legend>
+					<select>
+						{ field.options.map( ( option, index ) => (
+							<option key={ 'contact-form-select-option-' + index }>{ option }</option>
+						) ) }
+					</select>
+				</fieldset>
+			);
+		}
+		const radio = ( field, index ) => {
+			return (
+				<fieldset key={ 'contact-form-field-' + index } className="wpview-type-contact-form-field">
+					<legend>{ field.label }{ required( field.required ) }</legend>
+						{ field.options.map( ( option, index ) => (
+							<label key={ 'contact-form-radio-' + index }><input type="radio" /><span>{ option }</span></label>
+						) ) }
+				</fieldset>
+			);
+		}
+
+		const renderField = {
+			name: textField,
+			email: textField,
+			url: textField,
+			text: textField,
+			textarea,
+			checkbox,
+			dropdown
+		};
+
+		const { fields } = deserialize( this.props.content );
+		if ( fields && Array.isArray( fields ) ) {
+			return fields.map( ( field, index ) => field.type in renderField ? renderField[field.type]( field, index ) : null )
+		}
+
+		return null;
+	},
+
 	render() {
 		return (
 			<div className="wpview-content wpview-type-contact-form">
-				<p>This is a placeholder for the form preview.</p>
+				{ this.renderContent() }
+				<button disabled>{ this.translate( 'Submit' ) }</button>
 			</div>
 		);
 	}
