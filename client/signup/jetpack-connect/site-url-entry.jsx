2fb772746b855a19e342c0bf964bff5ffc705e1e
alternatekev <alternatekev@me.com>
2016-02-17 15:51:39 -0700

    added stub views for signup flow

---
+++ client/signup/jetpack-connect/site-url-entry.jsx
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react';
+
+import Button from 'components/button';
+import FormTextInput from 'components/forms/form-text-input';
+
+export default React.createClass( {
+	displayName: 'JetpackConnectSiteURLEntry',
+
+	propTypes: {
+
+	},
+
+	render() {
+		return (
+			<div className="jetpack-connect__site-url-entry-container">
+
+			</div>
+		);
+	}
+} );
